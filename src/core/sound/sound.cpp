6e12e0b6a228c60852d6aafbe42c9a81d8ebbfff
Urist <captainurist@gmail.com>
2021-02-07 14:13:00 +0300

    Async sound!

--- src/core/sound/sound.cpp
+++ src/core/sound/sound.cpp
@@ -12,7 +12,6 @@ Sound::Sound(const QString& id, const QString& path):
 
 void Sound::play() {
     // TODO: Qt6 doesn't have QSound yet, so I'm just throwing a winapi call here.
-    // Also note that this call actually BLOCKS while playing the sound. Doh!
-    PlaySoundW(reinterpret_cast<LPCWSTR>(m_path.utf16()), NULL, SND_FILENAME);
+    PlaySoundW(reinterpret_cast<LPCWSTR>(m_path.utf16()), NULL, SND_FILENAME | SND_ASYNC);
 }
 
