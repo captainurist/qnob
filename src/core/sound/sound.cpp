f11b3ce18e181670d7e904f5fd9cf25c9e72ec70
Urist <captainurist@gmail.com>
2021-02-10 00:46:14 +0300

    TODO added

--- src/core/sound/sound.cpp
+++ src/core/sound/sound.cpp
@@ -12,6 +12,7 @@ Sound::Sound(const QString& id, const QString& path):
 
 void Sound::play() {
     // TODO: Qt6 doesn't have QSound yet, so I'm just throwing a winapi call here.
+    // TODO: also this one quacks right now on repeated key events.
     PlaySoundW(reinterpret_cast<LPCWSTR>(m_path.utf16()), NULL, SND_FILENAME | SND_ASYNC);
 }
 
