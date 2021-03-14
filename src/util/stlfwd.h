a74f5faf6e8cd9e978b36a219f60a4db73946fbb
Urist <captainurist@gmail.com>
2021-03-14 16:52:09 +0300

    Better support for STL containers in QVariant.

---
+++ src/util/stlfwd.h
@@ -0,0 +1,21 @@
+#pragma once
+
+
+namespace std {
+
+template<class T, class Allocator>
+class vector;
+
+template<class T, class Less, class Allocator>
+class set;
+
+template<class Key, class Value, class Less, class Allocator>
+class map;
+
+template<class T, class Hash, class Eq, class Allocator>
+class unordered_set;
+
+template<class Key, class Value, class Hash, class Eq, class Allocator>
+class unordered_map;
+
+}
--- src/util/variant.h
+++ src/util/variant.h
@@ -1,16 +1,21 @@
 #pragma once
 
 #include <cassert>
-#include <map>
-#include <vector>
-#include <unordered_map>
 
 #include <QtCore/QVariant>
 #include <QtCore/QMetaType>
 
+#include "stlfwd.h"
+
+/* QVariantList/Map/Hash use Qt containers, which are not STL compatible in a lot of weird ways.
+ * So we need our own containers. */
+
 using VariantVector = std::vector<QVariant>;
 using VariantMap = std::map<QString, QVariant>;
 using VariantHash = std::unordered_map<QString, QVariant>;
+
+
+/* And we also need some magic to have compile-time metatype ids for our custom container classes. */
 
 namespace MetaType {
     enum {
@@ -24,6 +29,37 @@ Q_DECLARE_BUILTIN_METATYPE("VariantVector", MetaType::VariantVector, VariantVect
 Q_DECLARE_BUILTIN_METATYPE("VariantMap", MetaType::VariantMap, VariantMap)
 Q_DECLARE_BUILTIN_METATYPE("VariantHash", MetaType::VariantHash, VariantHash)
 
+
+/* Also now that we've decided to use STL containers we need to make `QVariant` use its internal storage for them. */
+
+#define QB_DECLARE_MOVABLE_CONTAINER(CONTAINER)                                                                         \
+template<class... T>                                                                                                    \
+class QTypeInfo<CONTAINER<T...>> {                                                                                      \
+public:                                                                                                                 \
+    enum {                                                                                                              \
+        isPointer = false,                                                                                              \
+        isIntegral = false,                                                                                             \
+        isComplex = true,                                                                                               \
+        isRelocatable = true,                                                                                           \
+    };                                                                                                                  \
+};
+
+QB_DECLARE_MOVABLE_CONTAINER(std::vector)
+QB_DECLARE_MOVABLE_CONTAINER(std::map)
+QB_DECLARE_MOVABLE_CONTAINER(std::set)
+QB_DECLARE_MOVABLE_CONTAINER(std::unordered_map)
+QB_DECLARE_MOVABLE_CONTAINER(std::unordered_set)
+#undef QB_DECLARE_MOVABLE_CONTAINER
+
+
+// TODO: push upstream?
+/**
+ * Constructs a `QVariant` by moving a provided rvalue into its internal storage. Note that `QVariant` doesn't provide
+ * this functionality - it always performs a copy.
+ *
+ * \param value                         An rvalue to construct a `QVariant` from.
+ * \returns                             `QVariant` wrapping the provided value.
+ */
 template<class T>
 QVariant variantFromRValue(T&& value) {
     static_assert(!std::is_lvalue_reference_v<T>, "Expected an rvalue reference.");
@@ -33,6 +69,13 @@ QVariant variantFromRValue(T&& value) {
     return result;
 }
 
+/**
+ * This basically is a zerocopy read accessor for `QVariant`. Note that `QVariant` doesn't provide this functionality -
+ * it always performs a copy.
+ *
+ * \param value                         Variant to get a reference for,
+ * \returns                             Reference to the value stored inside a `QVariant`.
+ */
 template<class T>
 const T& variantValueRef(const QVariant& value) {
     assert(value.typeId() == qMetaTypeId<T>());
