71555ce4d7eca0b15c54d44183be3d92e7db1465
captainurist <73941350+captainurist@users.noreply.github.com>
2021-10-31 01:15:21 +0300

    Using xformat where appropriate

--- src/util/bad_cast_exception.cpp
+++ src/util/bad_cast_exception.cpp
@@ -2,14 +2,16 @@
 
 #include "human_readable_name.h"
 
+#include "format.h"
+
 BadCastException::BadCastException(const QString& message) :
     Exception(message)
 {}
 
 BadCastException::BadCastException(const std::type_info& from, const std::type_info& to) :
-    Exception(BadCastException::tr("Failed to convert %1 to %2").arg(humanReadableName(from)).arg(humanReadableName(to)))
+    Exception(xformat(BadCastException::tr("Failed to convert {} to {}"), humanReadableName(from), humanReadableName(to)))
 {}
 
 BadCastException::BadCastException(const QMetaType& from, const QMetaType& to):
-    Exception(BadCastException::tr("Failed to convert %1 to %2").arg(QLatin1String(from.name())).arg(QLatin1String(to.name())))
+    Exception(xformat(BadCastException::tr("Failed to convert {} to {}"), QLatin1String(from.name()), QLatin1String(to.name())))
 {}
