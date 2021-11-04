a147d6940e8de29924d1d127e9a83558649d81c7
Urist <73941350+captainurist@users.noreply.github.com>
2021-11-04 20:18:34 +0300

    Nice multi-line logging

--- src/lib/logging/file_logger.cpp
+++ src/lib/logging/file_logger.cpp
@@ -18,16 +18,27 @@ FileLogger::FileLogger(QFile* file) :
 }
 
 void FileLogger::log(QtMsgType type, const QMessageLogContext& context, const QString& message) {
-    // TODO: prefix before each \n
     QByteArray extendedMessage =
         m_prefixes[type] %
         QByteArrayLiteral(": ") %
-        message.toUtf8() %
-        QByteArrayLiteral(" (") %
         relativeSourcePath(context.file).data() %
         QByteArrayLiteral(":") %
         QByteArray::number(context.line) %
-        QByteArrayLiteral(")\n");
+        QByteArrayLiteral(": ");
+
+    qsizetype pos = 0;
+    while (pos < message.size()) {
+        qsizetype nextPos = message.indexOf(L'\n', pos);
+        if (nextPos == -1)
+            nextPos = message.size();
+
+        if (pos != 0)
+            extendedMessage += QByteArrayLiteral("    : ");
+
+        extendedMessage += QStringView(message.data() + pos, message.data() + nextPos).toUtf8();
+        extendedMessage += QByteArrayLiteral("\n");
+        pos = nextPos + 1;
+    }
 
     QMutexLocker locker(&m_mutex);
     m_file->write(extendedMessage);
