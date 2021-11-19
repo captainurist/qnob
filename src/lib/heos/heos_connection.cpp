a008fefb1f120eeec9206cdbfd371e1decae5a94
Urist <73941350+captainurist@users.noreply.github.com>
2021-11-19 23:01:04 +0300

    Heos connection draft.

--- src/lib/heos/heos_connection.cpp
+++ src/lib/heos/heos_connection.cpp
@@ -1,2 +1,54 @@
 #include "heos_connection.h"
 
+#include <QtNetwork/QTcpSocket>
+
+#include <util/debug.h>
+
+static const int HEOS_CLI_PORT = 1255;
+
+HeosConnection::HeosConnection(QObject* parent):
+    QObject(parent)
+{
+}
+
+HeosConnection::~HeosConnection() {}
+
+void HeosConnection::start(const QHostAddress& address) {
+    m_address = address;
+    m_socket.reset(new QTcpSocket(this));
+
+    connect(m_socket.get(), &QTcpSocket::connected, this, &HeosConnection::handleSocketConnected);
+    connect(m_socket.get(), &QTcpSocket::errorOccurred, this, &HeosConnection::handleSocketErrorOccured);
+    connect(m_socket.get(), &QTcpSocket::readyRead, this, &HeosConnection::handleSocketBytesAvailable);
+
+    m_socket->connectToHost(address, HEOS_CLI_PORT);
+}
+
+void HeosConnection::stop() {
+    m_socket->reset();
+}
+
+QByteArray HeosConnection::sendCommand(const QByteArray& command) {
+    while (m_socket->state() != QTcpSocket::ConnectedState)
+        qApp->processEvents();
+
+    m_socket->write(command + "\r\n");
+    m_socket->flush();
+
+    while(!m_socket->bytesAvailable())
+        qApp->processEvents();
+
+    return QByteArray();
+}
+
+void HeosConnection::handleSocketConnected() {
+
+}
+
+void HeosConnection::handleSocketErrorOccured() {
+    xWarning("Could not connect to HEOS host {}", m_address.toString());
+}
+
+void HeosConnection::handleSocketBytesAvailable() {
+    xInfo("{}", m_socket->readAll());
+}
--- src/lib/heos/heos_connection.h
+++ src/lib/heos/heos_connection.h
@@ -1,14 +1,36 @@
 #pragma once
 
+#include <memory>
 
-// V 1.13 cli protocol
-// https://rn.dmglobal.com/euheos/HEOS_CLI_ProtocolSpecification.pdf
+#include <QtCore/QObject>
+#include <QtNetwork/QHostAddress>
 
-class HeosConnection {
+class QTcpSocket;
+
+/**
+ * Connection to a HEOS device.
+ *
+ * See v1.13 spec at https://rn.dmglobal.com/euheos/HEOS_CLI_ProtocolSpecification.pdf.
+ */
+class HeosConnection: public QObject {
+    Q_OBJECT
 public:
-    //HeosConnection(const QHostAddress&);
+    HeosConnection(QObject* parent);
+    virtual ~HeosConnection();
 
+    void start(const QHostAddress& address);
+
+    void stop();
+
+    QByteArray sendCommand(const QByteArray& command);
 
 private:
+    void handleSocketConnected();
+    void handleSocketErrorOccured();
+    void handleSocketBytesAvailable();
 
+private:
+    std::unique_ptr<QTcpSocket> m_socket;
+    QHostAddress m_address;
 };
+
